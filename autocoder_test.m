972e776e2f93e564ab3d93d43510fc329db61f5b
prm14 <prm@datrain.cn>
2015-11-19 20:05:49 +0800

    a

--- autocoder_test.m
+++ autocoder_test.m
@@ -1,12 +1,12 @@
 clc;clear all;close all;
 n=2;
-% data_train0=[%eye(2^n);%rand(7,2^n);
-%     .1 .2 .3 .4
-%     .1 .2 .3 .5
-%     .2 .2 .3 .4
-%     .3 .2 .1 .5
-%     ];
-data_train0=eye(2^n);%rand(7,2^n);
+data_train0=[%eye(2^n);%rand(7,2^n);
+    .1 .2 .3 .4
+    .1 .2 .3 .5
+    .2 .2 .3 .4
+    .3 .2 .1 .5
+    ];
+% data_train0=eye(2^n);%rand(7,2^n);
 label0=[0 1;0 1;1 0;1 0];
 % data_train0=[data_train0;[1 0 0 .1]];
 % data_train0(1,1:2)=[1,0];
@@ -22,7 +22,7 @@ label1=repmat(label0,10000,1);
 % [dataout,median_out]=autocoder_reconstruction(data_train0,w1,b1,b2);
 
 %% dnn
-layer=[2*n fn];
+layer=[2*n n];
 maxepoch=200;
 args.layer=layer;
 args.maxepoch=maxepoch;
